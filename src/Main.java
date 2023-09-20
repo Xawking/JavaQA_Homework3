57e632a3f56827177128ea0000b38bc2e50478e0
RayanGosling <mr.avdonin@inbox.ru>
2023-09-20 13:59:25 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int TicketCost = 13676;
+        int MoneyPerMile = 20;
+        int BonusMile = TicketCost / MoneyPerMile;
+        System.out.println("Количество бонусных миль =" + BonusMile);
+
+    }
+}
